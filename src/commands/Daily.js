9426c47fc5e10e24ecf20019ddf5430cf8405383
im2rnado <yassin.bedier@gmail.com>
2020-11-24 13:09:33 +0200

    Update Daily.js

--- src/commands/Daily.js
+++ src/commands/Daily.js
@@ -9,98 +9,98 @@ module.exports = {
 	name: 'daily',
 	description: 'Claims Daily STW Reward (Premium Only)',
 	aliases: ['claim', 'stw'],
-	async execute(message, args) {
+	async execute(message, args, client) {
 		const tagName = message.author.id;
 
-			// DMs only
-			if (message.guild) {
-				return message.channel.send('This command only works in DMs.').then(m => m.delete({ timeout: 3900 }))
-					.catch(err => {
-						console.log(err);
-					});
-			}
+		// DMs only
+		if (message.guild) {
+			return message.channel.send('This command only works in DMs.').then(m => m.delete({ timeout: 3900 }))
+				.catch(err => {
+					console.log(err);
+				});
+		}
 
-			const h = await message.channel.send('Claiming Daily Reward ...');
+		const h = await message.channel.send('Claiming Daily Reward ...');
 
-			try {
-				if (fs.existsSync(path)) {
+		try {
+			if (fs.existsSync(path)) {
 
-					const auth = new Auth();
+				const auth = new Auth();
 
-					const token = await auth.login(null, '');
-					console.log(token);
-					const { accountId } = require('../libs/deviceAuthDetails.json');
+				const token = await auth.login(null, '');
+				console.log(token);
+				const { accountId } = require('../libs/deviceAuthDetails.json');
 
-                                        // Get Kairos Color
-					let kcolor = client.sessions.get(`kcolor${tagName}`);
+				// Get Kairos Color
+				let kcolor = client.sessions.get(`kcolor${tagName}`);
 
-					if (!kcolor) {
-						const response34 = await axios.post(`https://channels-public-service-prod.ol.epicgames.com/api/v1/user/setting?accountId=${accountId}&settingKey=avatar&settingKey=avatarBackground`, {}, { headers: {
-							'Content-Type': 'application/json',
-							'Authorization': `Bearer ${token.access_token}`,
-						} }).catch((err) => {
-							console.error(err);
-						});
-
-						client.sessions.set(`kairos${tagName}`, response34.data[0].value);
-						client.sessions.set(`kcolor${tagName}`, JSON.parse(response34.data[1].value));
-						client.sessions.set(tagName, token.displayName);
-					}
-
-					kcolor = client.sessions.get(`kcolor${tagName}`);
-
-					// Get Display Name
-					const display1 = client.sessions.get(tagName);
-
-					if (!display1) {
-						return h.edit('❌ Could not find your account info.');
-					}
-
-					// Get Kairos Avatar
-					const kairos = client.sessions.get(`kairos${tagName}`);
-
-					if (!kairos) {
-						return h.edit('❌ Could not find your account info.');
-					}
-
-					const embed = new MessageEmbed().setColor(`${kcolor[1]}`);
-
-					const response = await axios.post(`${PUBLIC_BASE_URL}/game/v2/profile/${accountId}/client/ClaimLoginReward?profileId=campaign&rvn=-1`, {}, { headers: {
+				if (!kcolor) {
+					const response34 = await axios.post(`https://channels-public-service-prod.ol.epicgames.com/api/v1/user/setting?accountId=${accountId}&settingKey=avatar&settingKey=avatarBackground`, {}, { headers: {
 						'Content-Type': 'application/json',
-						'Authorization': `Bearer ${token}`,
+						'Authorization': `Bearer ${token.access_token}`,
 					} }).catch((err) => {
 						console.error(err);
-						h.edit('❌ You do not own STW!');
 					});
-					const notification = response.data.notifications[0];
-					console.log(notification);
 
-					const items = notification.items;
+					client.sessions.set(`kairos${tagName}`, response34.data[0].value);
+					client.sessions.set(`kcolor${tagName}`, JSON.parse(response34.data[1].value));
+					client.sessions.set(tagName, token.displayName);
+				}
 
-					if (items.length === 0) {
-						embed.setTitle('❎ You have already claimed today\'s reward!');
-						embed.setAuthor(`${display1}`, `https://cdn2.unrealengine.com/Kairos/portraits/${kairos}.png`);
-						embed.addField('Days Logged in', `**${notification.daysLoggedIn}**`);
-						return h.edit('', { embed: embed });
-					}
+				kcolor = client.sessions.get(`kcolor${tagName}`);
 
-					const reward = notification.items[0].itemType;
-					const quan = notification.items[0].quantity;
+				// Get Display Name
+				const display1 = client.sessions.get(tagName);
 
-					embed.setTitle('✅ Successfully Claimed Daily Reward');
+				if (!display1) {
+					return h.edit('❌ Could not find your account info.');
+				}
+
+				// Get Kairos Avatar
+				const kairos = client.sessions.get(`kairos${tagName}`);
+
+				if (!kairos) {
+					return h.edit('❌ Could not find your account info.');
+				}
+
+				const embed = new MessageEmbed().setColor(`${kcolor[1]}`);
+
+				const response = await axios.post(`${PUBLIC_BASE_URL}/game/v2/profile/${accountId}/client/ClaimLoginReward?profileId=campaign&rvn=-1`, {}, { headers: {
+					'Content-Type': 'application/json',
+					'Authorization': `Bearer ${token.access_token}`,
+				} }).catch((err) => {
+					console.error(err);
+					h.edit('❌ You do not own STW!');
+				});
+				const notification = response.data.notifications[0];
+				console.log(notification);
+
+				const items = notification.items;
+
+				if (items.length === 0) {
+					embed.setTitle('❎ You have already claimed today\'s reward!');
 					embed.setAuthor(`${display1}`, `https://cdn2.unrealengine.com/Kairos/portraits/${kairos}.png`);
 					embed.addField('Days Logged in', `**${notification.daysLoggedIn}**`);
-					embed.addField('Claimed', `**${quan}** x **${reward}**`);
 					return h.edit('', { embed: embed });
 				}
-				else{
-					h.edit('❌ You are not logged in.');
-				}
+
+				const reward = notification.items[0].itemType;
+				const quan = notification.items[0].quantity;
+
+				embed.setTitle('✅ Successfully Claimed Daily Reward');
+				embed.setAuthor(`${display1}`, `https://cdn2.unrealengine.com/Kairos/portraits/${kairos}.png`);
+				embed.addField('Days Logged in', `**${notification.daysLoggedIn}**`);
+				embed.addField('Claimed', `**${quan}** x **${reward}**`);
+				return h.edit('', { embed: embed });
 			}
-			catch(err) {
-				console.error(err);
-				h.edit('❌ You do not own STW!');
+			else{
+				h.edit('❌ You are not logged in.');
 			}
+		}
+		catch(err) {
+			console.error(err);
+			h.edit('❌ You do not own STW!');
+		}
 
 	},
 };
