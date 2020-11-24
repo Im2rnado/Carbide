cb508556a5526d815c443b1f59d67f7f7369abcc
im2rnado <yassin.bedier@gmail.com>
2020-11-24 13:08:28 +0200

    Update Balance.js

--- src/commands/Balance.js
+++ src/commands/Balance.js
@@ -31,43 +31,43 @@ module.exports = {
 				console.log(token);
 				const { accountId } = require('../libs/deviceAuthDetails.json');
 
-					// Get Kairos Color
-					let kcolor = client.sessions.get(`kcolor${tagName}`);
+				// Get Kairos Color
+				let kcolor = client.sessions.get(`kcolor${tagName}`);
 
-					if (!kcolor) {
-						const response34 = await axios.post(`https://channels-public-service-prod.ol.epicgames.com/api/v1/user/setting?accountId=${accountId}&settingKey=avatar&settingKey=avatarBackground`, {}, { headers: {
-							'Content-Type': 'application/json',
-							'Authorization': `Bearer ${token.access_token}`,
-						} }).catch((err) => {
-							console.error(err);
-						});
+				if (!kcolor) {
+					const response34 = await axios.post(`https://channels-public-service-prod.ol.epicgames.com/api/v1/user/setting?accountId=${accountId}&settingKey=avatar&settingKey=avatarBackground`, {}, { headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${token.access_token}`,
+					} }).catch((err) => {
+						console.error(err);
+					});
 
-						client.sessions.set(`kairos${tagName}`, response34.data[0].value);
-						client.sessions.set(`kcolor${tagName}`, JSON.parse(response34.data[1].value));
-						client.sessions.set(tagName, token.displayName);
-					}
+					client.sessions.set(`kairos${tagName}`, response34.data[0].value);
+					client.sessions.set(`kcolor${tagName}`, JSON.parse(response34.data[1].value));
+					client.sessions.set(tagName, token.displayName);
+				}
 
-					kcolor = client.sessions.get(`kcolor${tagName}`);
+				kcolor = client.sessions.get(`kcolor${tagName}`);
 
-					// Get Display Name
-					const display1 = client.sessions.get(tagName);
+				// Get Display Name
+				const display1 = client.sessions.get(tagName);
 
-					if (!display1) {
-						return h.edit('❌ Could not find your account info.');
-					}
+				if (!display1) {
+					return h.edit('❌ Could not find your account info.');
+				}
 
-					// Get Kairos Avatar
-					const kairos = client.sessions.get(`kairos${tagName}`);
+				// Get Kairos Avatar
+				const kairos = client.sessions.get(`kairos${tagName}`);
 
-					if (!kairos) {
-						return h.edit('❌ Could not find your account info.');
-					}
+				if (!kairos) {
+					return h.edit('❌ Could not find your account info.');
+				}
 
 				const embed = new MessageEmbed().setColor(`${kcolor[1]}`);
 
 				const response = await axios.post(`${PUBLIC_BASE_URL}/game/v2/profile/${accountId}/client/QueryProfile?profileId=common_core&rvn=-1`, {}, { headers: {
 					'Content-Type': 'application/json',
-					'Authorization': `Bearer ${token}`,
+					'Authorization': `Bearer ${token.access_token}`,
 				} }).catch((err) => {
 					console.error(err);
 					h.edit('❌ An Error Has Occured');
@@ -84,6 +84,12 @@ module.exports = {
 
 						breakdown.push(`**${Number(item.quantity).toLocaleString()}** x **${item.attributes.platform || 'Shared'} ${item.templateId.split(':')[1].replace('Mtx', '')}**`);
 					}
+					else {
+						embed.setTitle('0 Vbucks');
+						embed.setAuthor(`${display1}`, `https://cdn2.unrealengine.com/Kairos/portraits/${kairos}.png`);
+
+						return h.edit('', embed);
+					}
 				}
 
 				embed.setTitle(`${mtxQantitys.fullCurrency} Vbucks`);
@@ -97,14 +103,14 @@ module.exports = {
 			}
 		}
 		catch (err) {
-			console.error(err);
+			console.log(err);
 			const errormessage1 = new MessageEmbed()
 				.setColor('#ffff00')
 				.setTitle('⚠️ **Uh Oh! That was unexpected!**')
-				.setDescription(`There seems to be an error and we're working on a fix!`)
+				.setDescription('There seems to be an error and we\'re working on a fix!')
 				.addField('Error Message: ', `\`\`\`js\n${err}\`\`\``);
 
-			h.edit('', errormessage1);
+			return h.edit('', errormessage1);
 		}
 	},
 };
