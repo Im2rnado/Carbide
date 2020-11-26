397d7d3991268f391cf177b86263b0e3db48cb05
im2rnado <yassin.bedier@gmail.com>
2020-11-26 20:17:15 +0000

    Update shop.js

--- src/functions/shop.js
+++ src/functions/shop.js
@@ -35,57 +35,112 @@ module.exports = {
 			canvasWidth = 2340; canvasHeight = Math.ceil(canvasHeight / 3) * 110;
 		}
 
-		const canvas = createCanvas(canvasWidth, canvasHeight, 'png');
-		const ctx = canvas.getContext('2d');
+		if (content.length < 90) {
 
-		// eslint-disable-next-line no-async-promise-executor
-		return new Promise(async (resolve) => {
+			const canvas = createCanvas(canvasWidth, canvasHeight, 'png');
+			const ctx = canvas.getContext('2d');
 
-			let featuredX = 0;
-			let featuredY = 0;
-			let rendered = 0;
-			let renderedlen = 0;
-			const background = await loadImage('./src/assets/background.png');
-			ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			// eslint-disable-next-line no-async-promise-executor
+			return new Promise(async (resolve) => {
 
-			for(const itemStack in content) {
-				const item = content[itemStack];
+				let featuredX = 0;
+				let featuredY = 0;
+				let rendered = 0;
+				let renderedlen = 0;
+				const background = await loadImage('./src/assets/background.png');
+				ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-				const itemImg = await loadImage(`${item}background_full.v2.en.png`).catch((err) => { console.error(err); });
+				for(const itemStack in content) {
+					const item = content[itemStack];
 
-				ctx.drawImage(itemImg, featuredX, featuredY, 260, 260);
+					const itemImg = await loadImage(`${item}background_full.v2.en.png`).catch((err) => { console.error(err); });
 
-				featuredX = featuredX + 260;
-				rendered = rendered + 1;
-				if (content.length < 10) {renderedlen = 4;}
-				else if (content.length < 20) {renderedlen = 5;}
-				else if (content.length < 30) {renderedlen = 6;}
-				else if (content.length < 40) {renderedlen = 7;}
-				else if (content.length < 50) {renderedlen = 8;}
-				else { renderedlen = 9; }
-				if(rendered === renderedlen) {
-					rendered = 0;
-					featuredY = featuredY + 260;
-					featuredX = 0;
+					ctx.drawImage(itemImg, featuredX, featuredY, 260, 260);
+
+					featuredX = featuredX + 260;
+					rendered = rendered + 1;
+					if (content.length < 10) {renderedlen = 4;}
+					else if (content.length < 20) {renderedlen = 5;}
+					else if (content.length < 30) {renderedlen = 6;}
+					else if (content.length < 40) {renderedlen = 7;}
+					else if (content.length < 50) {renderedlen = 8;}
+					else { renderedlen = 9; }
+					if(rendered === renderedlen) {
+						rendered = 0;
+						featuredY = featuredY + 260;
+						featuredX = 0;
+					}
+
 				}
 
-			}
 
+				ctx.fillStyle = '#ffffff';
+				ctx.font = '135px Burbank Big Cd Bk';
+				ctx.textAlign = 'center';
+				ctx.fillText('www.carbide.cf', canvas.width / 2, canvas.height - 80);
 
-			ctx.fillStyle = '#ffffff';
-			ctx.font = '135px Burbank Big Cd Bk';
-			ctx.textAlign = 'center';
-			ctx.fillText('www.carbide.cf', canvas.width / 2, canvas.height - 80);
+				const end = fs.createWriteStream('./src/final/shop.png');
+				const stream = canvas.createPNGStream().pipe(end);
+				stream.on('finish', () => {
+					resolve('./src/final/shop.png');
+				});
 
-			const end = fs.createWriteStream('./src/final/shop.png');
-			const stream = canvas.createPNGStream().pipe(end);
-			stream.on('finish', () => {
-				resolve('./src/final/shop.png');
+			}).catch((err) => {
+				console.error(err);
 			});
+		}
+		else {
+			const canvas = createCanvas(canvasWidth, canvasHeight);
+			const ctx = canvas.getContext('2d');
 
-		}).catch((err) => {
-			console.error(err);
-		});
+			// eslint-disable-next-line no-async-promise-executor
+			return new Promise(async (resolve) => {
+
+				let featuredX = 0;
+				let featuredY = 0;
+				let rendered = 0;
+				let renderedlen = 0;
+
+				ctx.fillStyle = '#222528';
+				ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+				for(const itemStack in content) {
+					const item = content[itemStack];
+
+					const itemImg = await loadImage(`${item}background_full.v2.en.png`).catch((err) => { console.error(err); });
+
+					ctx.drawImage(itemImg, featuredX, featuredY, 260, 260);
+
+					featuredX = featuredX + 260;
+					rendered = rendered + 1;
+					if (content.length < 10) {renderedlen = 4;}
+					else if (content.length < 20) {renderedlen = 5;}
+					else if (content.length < 30) {renderedlen = 6;}
+					else if (content.length < 40) {renderedlen = 7;}
+					else if (content.length < 50) {renderedlen = 8;}
+					else { renderedlen = 9; }
+					if(rendered === renderedlen) {
+						rendered = 0;
+						featuredY = featuredY + 260;
+						featuredX = 0;
+					}
+
+				}
+
+
+				ctx.fillStyle = '#ffffff';
+				ctx.font = '135px Burbank Big Cd Bk';
+				ctx.textAlign = 'center';
+				ctx.fillText('www.carbide.cf', canvas.width / 2, canvas.height - 80);
+
+				const buf = await canvas.toBuffer('image/jpeg');
+
+				await resolve(fs.writeFileSync('./src/final/shop.jpeg', buf));
+
+			}).catch((err) => {
+				console.error(err);
+			});
+		}
 	},
 };
