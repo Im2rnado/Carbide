a4f207b9e05460a094f084f36b8f1049354b8a5d
im2rnado <yassin.bedier@gmail.com>
2020-11-26 20:16:43 +0000

    Update ShopImage.js

--- src/commands/ShopImage.js
+++ src/commands/ShopImage.js
@@ -10,8 +10,9 @@ module.exports = {
 	name: 'shopimage',
 	description: 'Returns Fortnite Shop',
 	aliases: ['sm', 'sp', 'si', 'shoppic', 'shopimage', 'shopi'],
+	category: 'General',
 	async execute(message) {
-		const h = await message.channel.send('Getting Battle Royale Shop ...');
+		const h = await message.channel.send('Getting Battle Royale Shop <a:loading:754479771089371318>');
 
 
 		fs.stat('./src/final/shop.png', async function(err) {
@@ -21,35 +22,50 @@ module.exports = {
 				return h.delete();
 			}
 			else {
-				const itemmss = [];
-				const response2 = await axios.get('https://fortniteapi.io/v1/shop?lang=en', { headers: {
-					'Content-Type': 'application/json',
-					'Authorization': '4bed3ab6-deb2685e-b3f6e8e5-16cd9f02',
-				} }).catch((err) => {
-					console.error(err);
+				fs.stat('./src/final/shop.jpeg', async function(err) {
+					if(!err) {
+						const attachment = new Discord.MessageAttachment('./src/final/shop.jpeg');
+						message.channel.send(`**🛒 Fortnite Item Shop | ${date}**`, attachment);
+						return h.delete();
+					}
+					else {
+						const itemmss = [];
+						const response2 = await axios.get('https://fortniteapi.io/v1/shop?lang=en', { headers: {
+							'Content-Type': 'application/json',
+							'Authorization': '4bed3ab6-deb2685e-b3f6e8e5-16cd9f02',
+						} }).catch((err) => {
+							console.error(err);
+						});
+
+						const speshal = response2.data.specialFeatured;
+						const daaly = response2.data.daily;
+						const dataaa = response2.data.featured;
+
+						await speshal.forEach(el => {
+							dataaa.push(el);
+						});
+
+						await daaly.forEach(el => {
+							dataaa.push(el);
+						});
+
+						await dataaa.forEach(el => {
+							itemmss.push(el.icon.split('transparent.png')[0]);
+						});
+
+						await genratateShop(itemmss);
+
+						let path;
+
+						if (fs.existsSync('./src/final/shop.png')) path = './src/final/shop.png';
+						else path = './src/final/shop.jpeg';
+
+						const attachment = new Discord.MessageAttachment(path);
+						message.channel.send(`**🛒 Fortnite Item Shop | ${date}**`, attachment);
+						h.delete();
+					}
+
 				});
-
-				const speshal = response2.data.specialFeatured;
-				const daaly = response2.data.daily;
-				const dataaa = response2.data.featured;
-
-				await speshal.forEach(el => {
-					dataaa.push(el);
-				});
-
-				await daaly.forEach(el => {
-					dataaa.push(el);
-				});
-
-				await dataaa.forEach(el => {
-					itemmss.push(el.icon.split('transparent.png')[0]);
-				});
-
-				await genratateShop(itemmss);
-
-				const attachment = new Discord.MessageAttachment('./src/final/shop.png');
-				message.channel.send(`**🛒 Fortnite Item Shop | ${date}**`, attachment);
-				h.delete();
 			}
 		});
 	},
